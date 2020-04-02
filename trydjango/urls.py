aa6b603f98c8086767d563a490f28e77301a0475
Tariqul islam <tariqulislamsourov@gmail.com>
2020-04-02 20:13:13 +0600

    second commit

---
+++ trydjango/urls.py
@@ -0,0 +1,32 @@
+"""trydjango URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from pages import views
+from products.views import product_detail_view
+from products.views import product_create_view, dynamic_lookup_view, product_list_view
+
+urlpatterns = [
+    path('',views.home_view, name= 'home'),
+    path('home/',views.home_view, name= 'home'),
+    path('about/',views.about_view),
+    path('contact/',views.contact_view),
+    path('admin/', admin.site.urls),
+    path('product/', product_detail_view),
+    path('product_form/', product_create_view),
+    path('products/<int:id>/', dynamic_lookup_view, name= 'product'),
+    path('products/', product_list_view)
+]
